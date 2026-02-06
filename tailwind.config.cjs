9f6d026143d4203ef4b7ca3d6c32d88b5aa9820d
choidaye8170 <gracechoidaye@gmail.com>
2026-02-06 22:41:10 +0900

    feat : 디자인 시스템 설정(폰트)

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        interop: ["Interop", "sans-serif"],
+        jetbrains: ["JetBrains Mono", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [],
+};
