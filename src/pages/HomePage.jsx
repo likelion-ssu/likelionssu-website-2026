c212d1911ea2e5cdf0edcc4f21f426ece8237b2c
choidaye8170 <gracechoidaye@gmail.com>
2026-02-06 23:32:13 +0900

    feat : 컬러명 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center gap-6 px-10">
-      <div className="typo-pretitle1k text-primary-brand">HomePage</div>
+      <div className="typo-pretitle1k text-primarybrand">HomePage</div>
 
       <nav className="flex flex-col gap-4">
         {["P A R T", "A B O U T U S", "P R O J E C T"].map((item) => (
