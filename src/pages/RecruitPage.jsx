63c0dffaa04f1162db311d18381e1afde6ef1172
ukyeong-user <hyk040520@naver.com>
2026-02-14 15:42:41 +0900

    feat: 기존 페이지와 동일하게 설정 완료 (리투크링 페이지의 경우 리크루팅 버튼 없음)

--- src/pages/RecruitPage.jsx
+++ src/pages/RecruitPage.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "../components/header/Header";
+import SideBar from "../components/sidebar/SideBar";
+import Footer from "../components/footer/Footer";
+
+import IntroSection from "../features/Recruit/components/IntroSection";
+import ValueSection from "../features/Recruit/components/ValueSection";
+import PartSection from "../features/Recruit/components/PartSection";
+import RoadmapSection from "../features/Recruit/components/RoadmapSection";
+import TimelineSection from "../features/Recruit/components/TimelineSection";
+import FaqSection from "../features/Recruit/components/FaqSection";
 
 export default function RecruitPage() {
-  return <div>RecruitPage</div>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
+  const closeSidebar = () => setIsSidebarOpen(false);
+
+  return (
+    <div className="bg-secondarybrand min-h-screen relative">
+      <Header noneSidebar={true} onMenuClick={toggleSidebar} />
+
+      <div className="pt-[7.8775rem] sm:pt-[7.3125rem]">
+        <IntroSection />
+        <ValueSection />
+        <PartSection />
+        <RoadmapSection />
+        <TimelineSection />
+        <FaqSection />
+      </div>
+
+      <Footer />
+      <SideBar isOpen={isSidebarOpen} onClose={closeSidebar} />
+    </div>
+  );
 }
