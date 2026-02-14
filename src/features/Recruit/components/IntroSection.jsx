b2d8e3e9cff0c3abcc9186d183d2122d6cc55652
ukyeong-user <hyk040520@naver.com>
2026-02-14 17:50:05 +0900

    feat: IntroSection 구현 완료(HomePage의 버튼과 동일한 위치를 위해 뷰포트 정중앙에 버튼 고정

--- src/features/Recruit/components/IntroSection.jsx
+++ src/features/Recruit/components/IntroSection.jsx
@@ -1,5 +1,65 @@
 import React from "react";
+import backgroundImg from "../assets/IntroSection_bg.svg";
+import button from "../assets/IntroSection_button.svg";
+import hoverButton from "../assets/IntroSection_hoverButton.svg";
 
 export default function IntroSection() {
-  return <div>IntroSection</div>;
+  return (
+    <div className="relative w-full min-h-screen bg-secondarybrand flex justify-center items-center px-4 sm:px-[3.75rem] pb-16 pt-[5.9375rem] sm:pt-[3.5625rem]">
+      {/* 좌측 상단 텍스트 - absolute로 그래픽 위치에 영향 없게 수정 */}
+      <div className="absolute left-4 sm:left-[3.75rem] top-[2.5rem] sm:top-[3.5625rem] flex flex-col z-10">
+        <h1 className="typo-recruit-title text-primarybrand">POSSIBILLITY</h1>
+        <h1 className="typo-recruit-title text-primarybrand">TO</h1>
+        <h1 className="typo-recruit-title text-primarybrand">REALITY</h1>
+        <p className="typo-recruit-subtitle text-primarybrand mt-4">
+          숭실대학교 멋쟁이사자처럼 14기 모집
+        </p>
+      </div>
+
+      {/* 중앙 그래픽 - 홈 버튼과 동일하게 뷰포트 정중앙 */}
+      <div className="flex justify-center items-center">
+        <div className="relative shrink-0">
+          {/* 배경 이미지 */}
+          <img
+            src={backgroundImg}
+            alt=""
+            className="
+              w-[28rem] h-[18rem]
+              sm:w-[min(28rem,70vw)]
+              sm:h-[min(18rem,45vh)]
+              object-contain
+            "
+          />
+
+          {/* 아이콘 (중앙 고정) - 호버 시 hoverButton 표시 */}
+          <div className="group absolute left-1/2 top-[48%] -translate-x-1/2 -translate-y-1/2 w-[4.9841rem] h-[4.9841rem] cursor-pointer">
+            <img
+              src={button}
+              alt=""
+              className="absolute inset-0 w-full h-full transition-opacity duration-200 group-hover:opacity-0"
+            />
+            <img
+              src={hoverButton}
+              alt=""
+              className="absolute inset-0 w-full h-full opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+            />
+          </div>
+
+          {/* 텍스트 (아이콘 오른쪽) */}
+          <p
+            className="
+              absolute
+              left-1/2
+              top-[48%]
+              -translate-y-1/2
+              translate-x-[5rem]
+              typo-recruit-scroll text-text whitespace-nowrap
+            "
+          >
+            모집 상세 보러 가기
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
