e1fb2a9ce53a128fcb1545c8e258c067c36316d0
choidaye8170 <gracechoidaye@gmail.com>
2026-02-15 16:15:49 +0900

    feat : 이미지 슬라이더 속도 수정 및 이미지 데이터 추가 저장

--- src/data/images.js
+++ src/data/images.js
@@ -10,7 +10,11 @@ import de4 from "../features/Part/assets/de/de-slide-4.svg";
 import fe1 from "../features/Part/assets/fe/fe-slide-1.svg";
 import fe2 from "../features/Part/assets/fe/fe-slide-2.svg";
 import fe3 from "../features/Part/assets/fe/fe-slide-3.svg";
-import be1 from "../features/Part/assets/be/be-slide-1.svg"
+import be1 from "../features/Part/assets/be/be-slide-1.svg";
+import be2 from "../features/Part/assets/be/be-slide-2.svg";
+import be3 from "../features/Part/assets/be/be-slide-3.svg";
+import be4 from "../features/Part/assets/be/be-slide-4.svg";
+import be5 from "../features/Part/assets/be/be-slide-5.svg";
 
 
 
@@ -18,5 +22,5 @@ export const sliderImages = {
   pm: [pm1, pm2, pm3],
   de: [de1, de2, de3, de4],
   fe: [fe1,fe2,fe3],
-  be: [be1],
+  be: [be1,be2,be3,be4,be5],
 };
