45e91da2c2ff444cfdaad69aeeae10517559a15d
ukyeong-user <hyk040520@naver.com>
2026-02-16 18:57:22 +0900

    chore: FilterSection에 cursor pointer 추가

--- src/features/Project/components/FilterSection.jsx
+++ src/features/Project/components/FilterSection.jsx
@@ -40,7 +40,7 @@ export default function FilterSection({
               key={tab}
               onClick={() => onTabChange(tab)}
               className={`
-                min-w-[5.5rem] sm:min-w-[7rem] px-3 py-2 sm:px-5 sm:py-3 border typo-buttontext transition-colors focus:outline-none focus:ring-0 focus:border-line
+                min-w-[5.5rem] sm:min-w-[7rem] px-3 py-2 sm:px-5 sm:py-3 border typo-buttontext transition-colors focus:outline-none focus:ring-0 focus:border-line cursor-pointer
                 ${isSticky ? "border-t-0" : ""}
                 ${
                   isSelected
